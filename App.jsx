9ad4f7e98e3d9705c21cf3ec149ebcbd70a72447
Abhishek Kumar <165244072+Abhixak@users.noreply.github.com>
2025-04-02 11:24:33 +0530

    Updated Routing

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {createBrowserRouter, createHashRouter, HashRouter, RouterProvider} from 'react-router-dom'
+import SideBar from './components/SideBar'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
+const routes=createHashRouter([
+  {
+    path:"/",
+    element:<About/>
+  },
+  {
+    path:"/projects",
+    element:<Projects/>
+  },
+  {
+    path:"/contact",
+    element:<Contact/>
+  }
+])
+
+const App = () => {
+  return (
+    <div className='flex'>
+      <SideBar/>
+      <RouterProvider router={routes} />
+    </div>
+  )
+}
+
+export default App
