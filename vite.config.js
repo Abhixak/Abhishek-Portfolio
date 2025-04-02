5e39f0354c3aa6cef13c14ffd2432f5b26d99d04
Abhishek Kumar <165244072+Abhixak@users.noreply.github.com>
2025-04-02 12:11:29 +0530

    MULTIPLE CHANGES TO FILE

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: 'Abhishek-Portfolio',
-  server: {
-    headers: {
-      "Permissions-Policy": "interest-cohort=()"
-    }
-  }
-  
+
 });
